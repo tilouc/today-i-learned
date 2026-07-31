86366eb0534ea0f7ec2de68f08c2a5376a2fe1a9
louis camara <imlouiscamara@gmail.com>
2026-07-31 00:17:35 +0000

    add functions for greeting and age checking, demonstrating positional and keyword arguments, default parameters, and return values

--- python/python-course/02_python_basics_2/main.py
+++ python/python-course/02_python_basics_2/main.py
@@ -345,7 +345,75 @@ show_tree()
 def say_hello(name, emoji):
     print(f'hello {name} {emoji}')
 
-#arguments
+# positional arguments
 say_hello('tilou', '☺️') #call, invoke
 say_hello('daniel', '☺️')
 say_hello('emilie', '☺️')
+
+# keyword arguments
+# say_hello(emoji='☺️', name='bibi') # bad practice
+say_hello(name='bibi', emoji='☺️')
+
+# default parameters
+def say_hello(name='darth vader', emoji='😈'):
+    print(f'hello {name} {emoji}')
+
+say_hello()
+say_hello('timmy')
+
+# return
+
+def sum(num1, num2):
+    return num1 + num2
+
+# a function should:
+# do one thing really well.
+# return something.
+# good practice
+
+# print(sum(4,5))
+# print(sum(10,5))
+total = sum(10,5) # 15
+print(sum(10,total))
+print(sum(10,15))
+print(sum(10,sum(10,5)))
+
+def sum(num1,num2):
+    def another_funct(n1,n2):
+        return n1 + n2
+    return another_funct(num1,num2)
+    # return automatically exits the function
+
+
+total = sum(10,20)
+print(total)
+
+# exercise tesla
+
+age = input("What is your age?: ")
+
+if int(age) < 18:
+	print("Sorry, you are too young to drive this car. Powering off")
+elif int(age) > 18:
+	print("Powering On. Enjoy the ride!");
+elif int(age) == 18:
+	print("Congratulations on your first year of driving. Enjoy the ride!")
+
+#1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+#2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+#checkDriverAge(92);
+#it returns "Powering On. Enjoy the ride!"
+#also make it so that the default age is set to 0 if no argument is given.
+
+def checkDriveAge(age=0):
+
+    if int(age) < 18:
+        return "Sorry, you are too young to drive this car. Powering off"
+    elif int(age) > 18:
+        return "Powering On. Enjoy the ride!"
+    elif int(age) == 18:
+        return "Congratulations on your first year of driving. Enjoy the ride!"
+
+print(checkDriveAge(20))
