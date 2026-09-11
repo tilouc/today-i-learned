2e3c4de5ad73ac99190e8be6324bfb846a8b4f79
tilouc <imlouiscamara@gmail.com>
2026-09-11 12:47:30 +0000

    refactor: simplify digit frequency score calculation in leetcode problem 3945

--- leetcode/3945_digit_frequency_score.py
+++ leetcode/3945_digit_frequency_score.py
@@ -1,21 +1,10 @@
 class Solution:
     def digitFrequencyScore(self, n: int) -> int:
         
-        n_str = str(n)
+        score = 0
 
-        n_str_l = []
+        while n:
+            score += n % 10
+            n //= 10
 
-        for i in range(len(n_str)):
-            n_str_l.append(int(n_str[i]))
-        
-        n_str_s = set(n_str_l)
-
-        n_str_sl = list(n_str_s)
-
-        n_str_lf = []
-
-        for j in range(len(n_str_sl)):
-            n_str_sl[j] = n_str_sl[j] * n_str_l.count(n_str_sl[j])
-            n_str_lf.append(n_str_sl[j])
-        
-        return sum(n_str_lf)
+        return score
