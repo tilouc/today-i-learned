246e3c40aa3cb6327bbff9d2d77cefb05a12238e
tilouc <imlouiscamara@gmail.com>
2026-09-14 20:49:06 +0000

    add: implement pets management with walking functionality in oop example

--- python/python-course/04_advanced_python_oop/main.py
+++ python/python-course/04_advanced_python_oop/main.py
@@ -270,3 +270,52 @@ for char in [wizard1, archer1]:
     char.attack()
 
 print(wizard1.attack())
+
+# Exercise Pets Everywhere
+
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+#1 Add another Cat
+class Blacky(Cat):
+    pass
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+
+simon = Simon('simon', 2)
+sally = Sally('sally', 3)
+blacky = Blacky('blacky', 4)
+
+my_cats = [simon, sally, blacky]
+
+#3 Instantiate the Pet class with all your cats use variable my_pets
+
+my_pets = Pets(my_cats)
+
+#4 Output all of the cats walking using the my_pets instance
+
+print(my_pets.walk())
